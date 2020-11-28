91929611a85965b609a0b72f0f1c2e2284c0af0c
bernadetteveni <49590735+bernadetteveni@users.noreply.github.com>
2020-11-28 16:55:49 -0600

    Final ajaxDepartment.php

--- Covid BusinessPro (Final Copy with Edits)/Sign up/ajaxDepartment.php
+++ Covid BusinessPro (Final Copy with Edits)/Sign up/ajaxDepartment.php
@@ -3,12 +3,12 @@
     $email= $_GET['email'];
     $shortemail= substr($email, strrpos($email, '@' )+1);
     $auth= $_GET['auth'];
-    $sql = "SELECT Departments.department FROM userRegister , Departments, Corporate WHERE (userRegister.email LIKE '%$shortemail' AND Departments.bid=Corporate.bid AND userRegister.accountType = '1' AND Corporate.accountAuthorization ='$auth')";
+    $sql = "SELECT Departments.department FROM userRegister , Departments, Corporate WHERE (userRegister.email LIKE '%$shortemail' AND Departments.bid=Corporate.bid AND  userRegister.uid = Departments.uid AND userRegister.accountType = '1' AND Corporate.accountAuthorization ='$auth')";
     $result= mysqli_query($conn, $sql); //execute query with bind parameter
     $department= "";
     if (mysqli_num_rows($result) > 0) {
         while($row = mysqli_fetch_assoc($result)){
-            $department .="<option value=".$row["department"].">".$row["department"]."</option>";
+            $department .="<option value='".$row['department']."'>".$row['department']."</option>";
         }
     }
     echo $department;
