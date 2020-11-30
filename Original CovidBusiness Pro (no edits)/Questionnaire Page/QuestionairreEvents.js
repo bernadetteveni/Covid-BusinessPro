232f7970d65f6c358e5c0cdc937e13c79047e710
bernadetteveni <49590735+bernadetteveni@users.noreply.github.com>
2020-11-29 21:40:48 -0600

    Delete QuestionairreEvents.js

--- Original CovidBusiness Pro (no edits)/Questionnaire Page/QuestionairreEvents.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("surveyButton").addEventListener("click", survey, false);
-document.getElementById("locationsButton").addEventListener("click", locations, false);
-document.getElementById("finishedSurvey").addEventListener("click", finishedSurvey, false);
-document.getElementById("finishedLocations").addEventListener("click", finishedLocations, false);
