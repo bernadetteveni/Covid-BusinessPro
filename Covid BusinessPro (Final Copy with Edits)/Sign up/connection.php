401047afb317af83963026852cd7f6eb7d72cb64
bernadetteveni <49590735+bernadetteveni@users.noreply.github.com>
2020-12-08 15:16:29 -0600

    Adjusted for transfer from server to hercules

--- Covid BusinessPro (Final Copy with Edits)/Sign up/connection.php
+++ Covid BusinessPro (Final Copy with Edits)/Sign up/connection.php
@@ -1,9 +1,9 @@
 <?php
     $servername="localhost";
-    $username="ense374";
-    $password="Ense374team#";
-    $dbname="CovidApp";
-    $conn= new mysqli('localhost','ense374','Ense374team#','CovidApp');
+    $username="veninatb";
+    $password="Quincy7";
+    $dbname="veninatb";
+    $conn= new mysqli('localhost','veninatb','Quincy7','veninatb');
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
